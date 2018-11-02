e1c54e89539a825822bf5b8057c8487093c22e31
briannagerold <bgerold@css.edu>
2018-11-02 09:41:43 -0500

    Request 407: Read the trades from a remote call to a web service

---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,40 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    class URLTradeDataProvider : ITradeDataProvider
+    {
+        private String url;
+
+        public URLTradeDataProvider(String url)
+        {
+            this.url = url;
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<string>();
+            // create a web client and use it to read the file stored at the given URL
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
+
+       
+    }
+}
